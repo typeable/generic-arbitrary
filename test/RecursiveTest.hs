c58b7fc362e90cbdb3521ffd6fe08574a956426d
Aleksey Uymanov <s9gf4ult@gmail.com>
2021-07-29 15:57:09 +0500

    add failing tests

---
+++ test/RecursiveTest.hs
@@ -0,0 +1,23 @@
+module RecursiveTest where
+
+import           Control.DeepSeq
+import           GHC.Generics                      (Generic)
+import           Test.QuickCheck
+import           Test.QuickCheck.Arbitrary.Generic
+
+data Expr
+  = Lit Int
+  | Add Expr Expr
+  | Mul Expr Expr
+  deriving (Eq, Show, Generic)
+
+-- | Instance with may fall into infinite loop
+instance Arbitrary Expr where
+  arbitrary = genericArbitrary
+  shrink = genericShrink
+
+instance NFData Expr
+
+-- | Check that we are not generating infinite
+prop_exprTotal :: Expr -> Property
+prop_exprTotal = total
