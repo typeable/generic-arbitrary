f75cf6e21478cd0c84ffc00fe7088d21f9221a03
Aleksey Uymanov <s9gf4ult@gmail.com>
2021-07-30 16:43:25 +0500

    recursion detection works

--- src/Test/QuickCheck/Arbitrary/Generic.hs
+++ src/Test/QuickCheck/Arbitrary/Generic.hs
@@ -52,7 +52,7 @@ newtype GenericArbitrary a = GenericArbitrary { unGenericArbitrary :: a }
 
 instance
   ( Generic a,
-    GArbitrary (Rep a) (Rep a),
+    GArbitrary a (Rep a),
     RecursivelyShrink (Rep a),
     GSubterms (Rep a) a
   ) => Arbitrary (GenericArbitrary a) where
@@ -64,35 +64,45 @@ class Finite a where
   -- | Generates final element, when size = 0.
   final :: QC.Gen a
 
-class GArbitrary root a where
-  gArbitrary :: Proxy root -> QC.Gen (a x)
+class GArbitrary self a where
+  gArbitrary :: Proxy self -> QC.Gen (a x)
 
 -- | Unit type instance
-instance GArbitrary root G.U1 where
+instance GArbitrary self G.U1 where
   gArbitrary _ = pure G.U1
 
 -- | Data of the constructor field
-instance Arbitrary c => GArbitrary root (Rec0 c) where
+instance Arbitrary c => GArbitrary self (Rec0 c) where
   gArbitrary _ = G.K1 <$> arbitrary
 
--- | Data type meta information
-instance GArbitrary (D1 t f) f => GArbitrary (D1 t f) (D1 t f) where
-  gArbitrary _ = G.M1 <$> gArbitrary (Proxy @(D1 t f))
+-- | Data type with single constructor
+instance
+  ( AllFieldsFinal self f ~ 'True
+  , GArbitrary self (C1 c f)
+  ) => GArbitrary self (D1 t (C1 c f)) where
+  gArbitrary _ = G.M1 <$> gArbitrary (Proxy @self)
+
+-- | Data type with multiple constructors
+instance
+  ( FinalConstructors self (a :+: b) ~ (x ': y)
+  , GArbitrary self (a :+: b)
+  ) => GArbitrary self (D1 t (a :+: b)) where
+  gArbitrary _ = G.M1 <$> gArbitrary (Proxy @self)
 
 -- | The constructor meta information
-instance GArbitrary root f => GArbitrary root (C1 c f) where
-  gArbitrary _ = G.M1 <$> scale predNat (gArbitrary (Proxy @root))
+instance GArbitrary self f => GArbitrary self (C1 c f) where
+  gArbitrary _ = G.M1 <$> scale predNat (gArbitrary (Proxy @self))
     where
       predNat 0 = 0
       predNat n = pred n
 
 -- | Constructor field meta information
-instance GArbitrary root f => GArbitrary root (S1 t f) where
-  gArbitrary _ = G.M1 <$> gArbitrary (Proxy @root)
+instance GArbitrary self f => GArbitrary self (S1 t f) where
+  gArbitrary _ = G.M1 <$> gArbitrary (Proxy @self)
 
 -- | Product
-instance (GArbitrary root a, GArbitrary root b) => GArbitrary root (a :*: b) where
-  gArbitrary _ = liftA2 (:*:) (gArbitrary (Proxy @root)) (gArbitrary (Proxy @root))
+instance (GArbitrary self a, GArbitrary self b) => GArbitrary self (a :*: b) where
+  gArbitrary _ = liftA2 (:*:) (gArbitrary (Proxy @self)) (gArbitrary (Proxy @self))
 
 -- | Calculates count of constructors encoded by particular ':+:'.
 -- Internal use only.
@@ -100,28 +110,28 @@ type family SumLen a :: Nat where
   SumLen (a G.:+: b) = (SumLen a) + (SumLen b)
   SumLen a           = 1
 
-type family FinalConstructors root (a :: * -> *) :: [* -> *] where
-  FinalConstructors root (a :+: b) =
-    FinalConstructors root a :++: FinalConstructors root b
-  FinalConstructors root (M1 C t f) = If (AllFieldsFinal root f) '[M1 C t f] '[]
+type family FinalConstructors self (a :: * -> *) :: [* -> *] where
+  FinalConstructors self (a :+: b) =
+    FinalConstructors self a :++: FinalConstructors self b
+  FinalConstructors self (M1 C t f) = If (AllFieldsFinal self f) '[M1 C t f] '[]
 
-type family AllFieldsFinal root (a :: * -> *) :: Bool where
-  AllFieldsFinal root U1 = 'True
-  AllFieldsFinal root (a :*: b) = AllFieldsFinal root a && AllFieldsFinal root b
-  AllFieldsFinal root (M1 S t (K1 R typ)) = Not (Equal (Rep typ) root)
+type family AllFieldsFinal self (a :: * -> *) :: Bool where
+  AllFieldsFinal self U1 = 'True
+  AllFieldsFinal self (a :*: b) = AllFieldsFinal self a && AllFieldsFinal self b
+  AllFieldsFinal self (M1 S t (K1 R self)) = 'False
+  AllFieldsFinal self (M1 S t (K1 R other)) = 'True
 
 -- | Sum
-instance ( (FinalConstructors root (a :+: b) ~ (x ': y))
-         , GArbitrary root a, GArbitrary root b
+instance ( GArbitrary self a, GArbitrary self b
          , KnownNat (SumLen a), KnownNat (SumLen b)
-         ) => GArbitrary root (a :+: b) where
+         ) => GArbitrary self (a :+: b) where
   gArbitrary _ = do
     sized $ \s ->
       if s > 1 then usual else final
     where
       usual = frequency
-        [ (lfreq, G.L1 <$> (gArbitrary (Proxy @root)))
-        , (rfreq, G.R1 <$> (gArbitrary (Proxy @root))) ]
+        [ (lfreq, G.L1 <$> (gArbitrary (Proxy @self)))
+        , (rfreq, G.R1 <$> (gArbitrary (Proxy @self))) ]
         where
           lfreq = fromIntegral $ natVal (Proxy :: Proxy (SumLen a))
           rfreq = fromIntegral $ natVal (Proxy :: Proxy (SumLen b))
@@ -129,6 +139,6 @@ instance ( (FinalConstructors root (a :+: b) ~ (x ': y))
 
 genericArbitrary
   :: forall a ga
-  . (Generic a, GArbitrary ga ga, ga ~ G.Rep a)
+  . (Generic a, GArbitrary a ga, ga ~ G.Rep a)
   => Gen a
-genericArbitrary = G.to <$> gArbitrary (Proxy @ga)
+genericArbitrary = G.to <$> gArbitrary (Proxy @a)
--- test/RecursiveTest.hs
+++ test/RecursiveTest.hs
@@ -18,6 +18,27 @@ instance Arbitrary Expr where
 
 instance NFData Expr
 
+data Single = Single
+  deriving (Eq, Show, Generic)
+
+instance Arbitrary Single where
+  arbitrary = genericArbitrary
+  shrink = genericShrink
+
+data Multiple = M1 | M2
+  deriving (Eq, Show, Generic)
+
+instance Arbitrary Multiple where
+  arbitrary = genericArbitrary
+  shrink = genericShrink
+
+data Fail = Fail Fail
+  deriving (Eq, Show, Generic)
+
+instance Arbitrary Fail where
+  arbitrary = genericArbitrary
+  shrink = genericShrink
+
 -- | Check that we are not generating infinite
 prop_exprTotal :: Expr -> Property
 prop_exprTotal = total
