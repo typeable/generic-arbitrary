949d4d958639e5f9e55d3711e8fc89cea2676ef3
Aleksey Uymanov <s9gf4ult@gmail.com>
2024-01-12 12:51:12 +0500

    enum test

---
+++ test/EnumTest.hs
@@ -0,0 +1,18 @@
+{-# OPTIONS_GHC -fconstraint-solver-iterations=6 #-}
+
+module EnumTest where
+
+import Control.DeepSeq
+import GHC.Generics
+import Test.QuickCheck
+import Test.QuickCheck.Arbitrary.Generic
+
+data Country = PL | GB | RU | RO | CZ | HR | SK | DE | NL | ES | BR
+  deriving (Generic, NFData, Show)
+
+instance Arbitrary Country where
+  arbitrary = genericArbitrary
+  shrink = genericShrink
+
+prop_CountryTest :: Country -> Property
+prop_CountryTest = total
